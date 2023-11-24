4ae8f7f008b723bd6ded01b23ce736a8f731bb43
CojitoJulio <dani.gutierrezg@duocuc.cl>
2023-11-24 01:48:31 -0300

    OOOOOOooou yeeeaaaa cacharron chacarron wauwauwauawuauauauauau

--- src/app/paginas/viaje/viaje.page.ts
+++ src/app/paginas/viaje/viaje.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { Geolocation } from '@capacitor/geolocation';
+import 'leaflet-routing-machine';
+import { RutaActual } from 'src/app/models/ruta-actual';
 
 @Component({
   selector: 'app-viaje',
@@ -12,6 +14,10 @@ export class ViajePage implements OnInit {
 
   alat!: number;
   alon!: number;
+  waypoints: L.LatLng[] = [];
+  map: any;
+  rutona!: RutaActual;
+  coordenadas: number[][] = [];
 
   ngOnInit() {
     this.obtenerUbicacion(); // Llama a obtenerUbicacion en ngOnInit
@@ -28,6 +34,31 @@ export class ViajePage implements OnInit {
       const map = L.map('map').setView([this.alat, this.alon], 17);
 
       this.CrearMapa(map);
+      this.coordenadas.push([this.alat, this.alon]);
+      this.getcoords();
+
+      this.waypoints = this.coordenadas.map((coord) =>
+        L.latLng(coord[0], coord[1])
+      );
+
+      // Crea la capa de la ruta
+      L.Routing.control({
+        waypoints: this.waypoints,
+      }).addTo(map);
+
+      var elementos = document.getElementsByClassName(
+        'leaflet-routing-alternatives-container'
+      );
+
+      // Itera sobre los elementos y cambia su estilo
+      for (var i = 0; i < elementos.length; i++) {
+        // Usa 'as HTMLElement' para indicar a TypeScript que estos elementos son de tipo HTMLElement
+        var elemento = elementos[i] as HTMLElement;
+
+        // Cambia el estilo según tus necesidades
+        elemento.style.display = 'none';
+        // Agrega más propiedades de estilo según sea necesario
+      }
     } catch (error) {
       console.error('Error al obtener la ubicación', error);
     }
@@ -41,5 +72,35 @@ export class ViajePage implements OnInit {
 
     // Agregar un marcador en la ubicación actual
     const marker = L.marker([this.alat, this.alon]).addTo(map);
+  }
+
+  AgregarRuta() {
+    // Agrega algunos puntos de ejemplo (latitud, longitud)
+    const puntos = [
+      [this.alat, this.alon],
+      [this.alat + 0.01, this.alon + 0.01],
+      [this.alat + 0.02, this.alon - 0.01],
+    ];
+
+    this.waypoints = puntos.map((coord) => L.latLng(coord[0], coord[1]));
+
+    // Crea la capa de la ruta
+    L.Routing.control({
+      waypoints: this.waypoints,
+    }).addTo(this.map);
+  }
+
+  getcoords() {
+    const ruta = localStorage.getItem('rutaselected');
+
+    if (ruta) {
+      this.rutona = JSON.parse(ruta);
+    }
+
+    this.rutona.clientes.forEach((cliente) => {
+      console.log(cliente);
+
+      this.coordenadas.push([cliente.lat, cliente.lon]);
+    });
   }
 }
